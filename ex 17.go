435cf47e2c077828728fd63e2cd0a8c989924a24
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:29:15 -0300

    Update ex 17.go

--- ex 17.go
+++ ex 17.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+    "fmt"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    count := make(map[rune]int)
+    for _, char := range str {
+        count[char]++
+    }
+
+    fmt.Print("Letras únicas: ")
+    for char, freq := range count {
+        if freq == 1 {
+            fmt.Printf("%c ", char)
+        }
+    }
+    fmt.Println()
+}
