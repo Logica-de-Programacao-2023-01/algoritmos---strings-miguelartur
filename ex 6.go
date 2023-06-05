94969abc3af85c4891cbf49c593980b82ef428ac
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:25:45 -0300

    Update ex 6.go

--- ex 6.go
+++ ex 6.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    words := strings.Fields(str)
+    count := len(words)
+
+    fmt.Printf("A string \"%s\" tem %d palavra(s).\n", str, count)
+}
