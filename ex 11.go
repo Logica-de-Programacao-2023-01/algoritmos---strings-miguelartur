ab6704548c892d58ba6992694d160f6e094c1f6b
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:27:24 -0300

    Update ex 11.go

--- ex 11.go
+++ ex 11.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    result := ""
+
+    for _, char := range str {
+        if !isVowel(char) {
+            result += string(char)
+        }
+    }
+
+    fmt.Printf("String sem vogais: %s\n", result)
+}
+
+func isVowel(char rune) bool {
+    switch char {
+    case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+        return true
+    default:
+        return false
+    }
+}
