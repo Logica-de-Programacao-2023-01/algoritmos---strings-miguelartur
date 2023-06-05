9f3e504dca2b8e0172d64fdddc816517a69feea7
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:24:33 -0300

    Update ex 2.go

--- ex 2.go
+++ ex 2.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Digite uma string:")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		output := strings.ReplaceAll(scanner.Text(), " ", "")
+		fmt.Println("String sem espaços em branco:", output)
+	}
+}
