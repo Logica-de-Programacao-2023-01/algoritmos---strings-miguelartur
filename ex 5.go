05bca04f2e07a36c0c8fd098265d26cc18f1ad41
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:25:29 -0300

    Update ex 5.go

--- ex 5.go
+++ ex 5.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var input string
+
+	fmt.Print("Digite um número em ponto flutuante: ")
+	fmt.Scan(&input)
+
+	if _, err := strconv.ParseFloat(input, 64); err == nil {
+		fmt.Println("É um número em ponto flutuante válido.")
+	} else {
+		fmt.Println("Não é um número em ponto flutuante válido.")
+	}
+}
