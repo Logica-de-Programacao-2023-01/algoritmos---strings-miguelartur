079b01c7a12c0dd374cd88c6db66f95e9c16d039
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:28:44 -0300

    Update ex 16.go

--- ex 16.go
+++ ex 16.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var str1, str2 string
+    fmt.Print("Digite a primeira string: ")
+    fmt.Scanln(&str1)
+    fmt.Print("Digite a segunda string: ")
+    fmt.Scanln(&str2)
+
+    if strings.Contains(str1, str2) {
+        fmt.Println("A segunda string é uma substring da primeira.")
+    } else {
+        fmt.Println("A segunda string não é uma substring da primeira.")
+    }
+}
