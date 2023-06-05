33b2a52435f355a4946267a33dc6c87e52dbc86c
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:30:03 -0300

    Update ex 20.go

--- ex 20.go
+++ ex 20.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+    "fmt"
+    "unicode"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    var count int
+    for i, char := range str {
+        if i == 0 {
+            if !unicode.IsUpper(char) {
+                fmt.Println("A string não está em camelCase.")
+                return
+            }
+            count++
+        } else if unicode.IsUpper(char) {
+            count++
+        }
+    }
+
+    fmt.Printf("A string está em camelCase e possui %d palavras.\n", count)
+}
