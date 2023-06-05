3011d0899d2e91c4dbea7b9a5c9982508bb58184
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:24:51 -0300

    Update ex 3.go

--- ex 3.go
+++ ex 3.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+
+    fmt.Print("Digite uma string: ")
+    scanner.Scan()
+    input := scanner.Text()
+
+    fmt.Print("Digite o caractere a ser substituído: ")
+    scanner.Scan()
+    oldChar := scanner.Text()
+
+    fmt.Print("Digite o novo caractere: ")
+    scanner.Scan()
+    newChar := scanner.Text()
+
+    output := strings.ReplaceAll(input, oldChar, newChar)
+
+    fmt.Println("String modificada:", output)
+}
