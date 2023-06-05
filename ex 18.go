8522df1fa599e9d030c39b6492a4bb34fcdedb87
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:29:30 -0300

    Update ex 18.go

--- ex 18.go
+++ ex 18.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+    "fmt"
+    "unicode"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    for _, char := range str {
+        if !unicode.IsDigit(char) {
+            fmt.Println("A string não contém apenas números.")
+            return
+        }
+    }
+
+    fmt.Println("A string contém apenas números.")
+}
