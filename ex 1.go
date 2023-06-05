710ce71861dbc6b3411ed87c48ecd6d23a4aaf79
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:24:13 -0300

    Update ex 1.go

--- ex 1.go
+++ ex 1.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Digite uma string:")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		output := strings.ToUpper(scanner.Text())
+		fmt.Println("String convertida:", output)
+	}
+}
