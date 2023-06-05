06cb2a052e78acb6ea96bd6350472c1a9a79b245
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:26:19 -0300

    Update ex 8.go

--- ex 8.go
+++ ex 8.go
@@ -1,1 +1,18 @@
+package main
 
+import (
+    "fmt"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    reversed := ""
+    for i := len(str)-1; i >= 0; i-- {
+        reversed += string(str[i])
+    }
+
+    fmt.Printf("A string \"%s\" invertida é \"%s\".\n", str, reversed)
+}
