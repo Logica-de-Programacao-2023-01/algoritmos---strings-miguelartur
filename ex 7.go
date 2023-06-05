92dfa9d81ed0e43ca038615c2a09b60b72c762ea
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:26:01 -0300

    Update ex 7.go

--- ex 7.go
+++ ex 7.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+    "fmt"
+    "unicode"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    hasNumber := false
+    for _, char := range str {
+        if unicode.IsDigit(char) {
+            hasNumber = true
+            break
+        }
+    }
+
+    if hasNumber {
+        fmt.Printf("A string \"%s\" contém pelo menos um número.\n", str)
+    } else {
+        fmt.Printf("A string \"%s\" não contém números.\n", str)
+    }
+}
