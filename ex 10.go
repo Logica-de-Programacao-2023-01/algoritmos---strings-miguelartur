56bd24ace8ea3ba6718a8f9a08e18835cb83550e
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:26:53 -0300

    Update ex 10.go

--- ex 10.go
+++ ex 10.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+    "fmt"
+    "sort"
+)
+
+func main() {
+    var str1, str2 string
+    fmt.Print("Digite a primeira string: ")
+    fmt.Scanln(&str1)
+    fmt.Print("Digite a segunda string: ")
+    fmt.Scanln(&str2)
+
+    str1Sorted := sortString(str1)
+    str2Sorted := sortString(str2)
+
+    if str1Sorted == str2Sorted {
+        fmt.Printf("\"%s\" e \"%s\" são anagramas.\n", str1, str2)
+    } else {
+        fmt.Printf("\"%s\" e \"%s\" não são anagramas.\n", str1, str2)
+    }
+}
+
+func sortString(str string) string {
+    runes := []rune(str)
+    sort.Slice(runes, func(i, j int) bool { return runes[i] < runes[j] })
+   
+    return string(runes)
+}
