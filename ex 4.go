572283987e9e772bce746cfff8d259dbff4e31d4
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:25:11 -0300

    Update ex 4.go

--- ex 4.go
+++ ex 4.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Digite a primeira string: ")
+	scanner.Scan()
+	str1 := scanner.Text()
+
+	fmt.Print("Digite a segunda string: ")
+	scanner.Scan()
+	str2 := scanner.Text()
+
+	if str1 == str2 {
+		fmt.Println("As strings são iguais.")
+	} else {
+		fmt.Println("As strings são diferentes.")
+	}
+}
