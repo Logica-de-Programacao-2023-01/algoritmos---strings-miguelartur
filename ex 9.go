8dcd9683b82a2f76f1d7547bbbb7f2c17611d84e
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:26:35 -0300

    Update ex 9.go

--- ex 9.go
+++ ex 9.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    var oldChar, newChar string
+    fmt.Print("Digite a letra a ser substituída: ")
+    fmt.Scanln(&oldChar)
+    fmt.Print("Digite a letra substituta: ")
+    fmt.Scanln(&newChar)
+
+    replaced := strings.Replace(str, oldChar, newChar, -1)
+
+    fmt.Printf("A string \"%s\" com \"%s\" substituído por \"%s\" é \"%s\".\n", str, oldChar, newChar, replaced)
+}
