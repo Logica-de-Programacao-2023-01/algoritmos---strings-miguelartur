73a209d475ecebcf345dcbba9abfa979f8eacfac
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:27:40 -0300

    Update ex 12.go

--- ex 12.go
+++ ex 12.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var palavra string
+	fmt.Println("Digite uma palavra:")
+	fmt.Scan(&palavra)
+
+	if isPalindromo(palavra) {
+		fmt.Println("A palavra é um palíndromo.")
+	} else {
+		fmt.Println("A palavra não é um palíndromo.")
+	}
+}
+
+func isPalindromo(palavra string) bool {
+	palavra = strings.ToLower(palavra)
+	for i := 0; i < len(palavra)/2; i++ {
+		if palavra[i] != palavra[len(palavra)-1-i] {
+			return false
+		}
+	}
+	return true
+}
