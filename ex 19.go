a886cff0bb99ea670110f2fc9d57487b88015b7e
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:29:48 -0300

    Update ex 19.go

--- ex 19.go
+++ ex 19.go
@@ -1,1 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var str string
+    fmt.Print("Digite uma string: ")
+    fmt.Scanln(&str)
+
+    runes := []rune(str)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+
+    fmt.Println(string(runes))
+}
 
