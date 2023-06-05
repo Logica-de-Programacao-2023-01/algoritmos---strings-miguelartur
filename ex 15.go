ca58950351205c95f219f13a82a8e2566ad99dae
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:28:28 -0300

    Update ex 15.go

--- ex 15.go
+++ ex 15.go
@@ -1,1 +1,17 @@
+package main
 
+import "fmt"
+
+func main() {
+	var palavra string
+	fmt.Println("Digite uma palavra:")
+	fmt.Scan(&palavra)
+
+	palavra = strings.ReplaceAll(palavra, "a", "*")
+	palavra = strings.ReplaceAll(palavra, "e", "*")
+	palavra = strings.ReplaceAll(palavra, "i", "*")
+	palavra = strings.ReplaceAll(palavra, "o", "*")
+	palavra = strings.ReplaceAll(palavra, "u", "*")
+
+	fmt.Println(palavra)
+}
