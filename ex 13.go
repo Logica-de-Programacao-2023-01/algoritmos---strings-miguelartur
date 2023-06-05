07c386bcad5bcd569914d5e7331927f9c76f02cc
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-05 09:27:58 -0300

    Update ex 13.go

--- ex 13.go
+++ ex 13.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var palavra string
+	fmt.Println("Digite uma palavra:")
+	fmt.Scan(&palavra)
+
+	if isCrescente(palavra) {
+		fmt.Println("A palavra é uma sequência numérica crescente.")
+	} else {
+		fmt.Println("A palavra não é uma sequência numérica crescente.")
+	}
+}
+
+func isCrescente(palavra string) bool {
+	for i := 0; i < len(palavra)-1; i++ {
+		num1, _ := strconv.Atoi(string(palavra[i]))
+		num2, _ := strconv.Atoi(string(palavra[i+1]))
+		if num1 >= num2 {
+			return false
+		}
+	}
+	return true
+}
